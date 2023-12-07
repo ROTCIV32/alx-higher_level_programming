4fb50dbefe13bd36afd7a2e84fcbfa95eeea2ea0
ROTCIV32 <kingvic255@gmail.com>
2023-12-06 19:33:53 -0800

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     from add_0 import add
-
     a = 1
     b = 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+# Calculate the result using the imported function
+result = add(a, b)
+
+# Print the result using string formatting
+print("{:d} + {:d} = {:d}".format(a, b, result))
